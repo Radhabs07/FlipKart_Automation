60841d6b15c38f8e24df0a64242686994c161fe2
RADHA BS <bs.radha07@gmail.com>
2024-05-09 13:19:03 +0530

    Updated Flipkart

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,18 +1,11 @@
 package demo;
 
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.NoSuchElementException;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -21,7 +14,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestCases {
     private ChromeDriver driver;
-    private FluentWait<WebDriver> wait;
+    //private FluentWait<WebDriver> wait;
     //@BeforeClass
     public TestCases()
     {
@@ -90,38 +83,38 @@ public class TestCases {
    // driver.get("https://www.flipkart.com/");
 
     // Locate the search input and enter "iPhone"
-    WebElement searchInput = driver.findElement(By.name("q"));
-    searchInput.sendKeys("iPhone");
-    searchInput.submit();
+   SeleniumWrapper wrapper=new SeleniumWrapper(driver);
 
-    // Find all product tiles on the page
-    //List<WebElement> productNames = driver.findElements(By.xpath("//div[@class='_4rR01T']//div[@class='_3Ay6Sb']"));
-    //WebDriverWait wait = new WebDriverWait(driver, 10);
-    //wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("//div[@class='_4rR01T']//div[@class='_3Ay6Sb']")));
+   wrapper.enterText(By.xpath("//input[@type='text']"), "Iphone");
+   wrapper.clickElement(By.xpath("//button[@type='submit']"));
+  
     
     List<WebElement> productNames = driver.findElements(By.xpath("//div[@class='_4rR01T']//div[@class='_3Ay6Sb']"));
-    System.out.println("Number of product tiles found: " + productNames.size());
-
-    
     //System.out.println("Number of product tiles found: " + productNames.size());
+
 
     // Iterate through each product tile and extract information
     for (WebElement productName : productNames) {
-        // Get the title of the product
-        WebElement titleElement = productName.findElement(By.className("_4rR01T"));
-        //Thread.sleep(2000);
-        String title = titleElement.getText();
+        // // Get the title of the product
+        // WebElement titleElement = productName.findElement(By.className("_4rR01T"));
+        // //Thread.sleep(2000);
+        // String title = titleElement.getText();
 
-        System.out.println("Product Name: " + title);
+        // System.out.println("Product Name: " + title);
 
         // Get the discount percentage
-        WebElement discountElement = productName.findElement(By.className("_3Ay6Sb"));
+        WebElement discountElement = productName.findElement(By.xpath("//div[@class='_3pLy-c row']//div[@class='_3Ay6Sb']"));
         //Thread.sleep(3000);
+
+        String title=" ";
+        int discount = 0;
         String discountText = discountElement.getText();
-        int discount = Integer.parseInt(discountText.replaceAll("[^0-9]", ""));
+         discount = Integer.parseInt(discountText.replaceAll("[^0-9]", ""));
 
         // Print the title and discount percentage if discount is more than 17%
-        if (discount > 17) {
+        if (discount >= 17) {
+
+          WebElement titleElement = productName.findElement(By.xpath("//div[@class='_3pLy-c row']//div[@class='_4rR01T']"));
             System.out.println("Title: " + title);
             System.out.println("Discount: " + discount + "%");
 
@@ -134,32 +127,31 @@ public class TestCases {
 
                            
         
-     @Test
+     //@Test
      public void testCase03() throws InterruptedException{
      System.out.println("Start Test case: testCase03");
 
      //Navigate to the website
-     //driver.get("https://www.flipkart.com/");
+    // driver.get("https://www.flipkart.com/");
+
+     SeleniumWrapper wrapper= new SeleniumWrapper(driver);
     
 
     // Locate the search input and enter "Coffee Mug"
-     WebElement searchInput = driver.findElement(By.xpath("//input[@type='text']"));
-     //Thread.sleep(2000);
-     searchInput.click();
-     //Thread.sleep(2000);
-     searchInput.sendKeys("Coffee Mug");
-     WebElement searchButton = driver.findElement(By.xpath("//button[@type='submit']"));
+      wrapper.enterText(By.xpath("//input[@type='text']"),"Coffee Mug");
+      WebElement searchButton = driver.findElement(By.xpath("//button[@type='submit']"));
      //((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", searchButton);
      //Thread.sleep(2000);
-     searchButton.click();
+      searchButton.click();
 
   
       try {
       List<WebElement> starCheckboxes = driver.findElements(By.xpath("//div[@class='_2d0we9']//div"));
-      for (WebElement checkbox : starCheckboxes) {
-          String starsText = checkbox.getText();
-          if (starsText.contains("4")) {
-              checkbox.click();
+      for (int i =0;i<starCheckboxes.size();i++) {
+          String starsText =starCheckboxes.get(i).getText();
+
+          if(starsText.contains("4")) {
+            starCheckboxes.get(i).click();
               break;
           }
       }
@@ -171,10 +163,11 @@ public class TestCases {
       Thread.sleep(2000);
 
       // Get the list of products with reviews
-      List<WebElement> products = driver.findElements(By.xpath("//div[@class='_1AtVbE']/a"));
+      List<WebElement> products = driver.findElements(By.xpath("//span[@class='_2_R_DZ']"));//a[@class='s1Q9rs']
+      int count=1;
       for (int i = 0; i < Math.min(5, products.size()); i++) {
           WebElement product = products.get(i);
-          String title = product.findElement(By.xpath(".//div[@class='IIdQZO _1SSAGr']/a/div")).getText();
+          String title = product.findElement(By.xpath("(//a[@class='s1Q9rs'])[" + (count + 1) + "]]/a/div")).getText();
           String imageURL = product.findElement(By.xpath(".//div[@class='CXW8mj']/img")).getAttribute("src");
           System.out.println("Title: " + title);
           System.out.println("Image URL: " + imageURL);
